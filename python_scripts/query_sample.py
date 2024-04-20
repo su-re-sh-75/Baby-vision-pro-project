4c31a73f4cef917ba5104cc077c645534346b72c
su-re-sh-75 <suresh22072003@gmail.com>
2024-04-20 19:20:36 +0530

    new file:   .gitignore 	new file:   config.ini 	modified:   influxdb.token 	new file:   python_scripts/query_sample.py 	new file:   python_scripts/write_sample.py 	new file:   requirements.txt

---
+++ python_scripts/query_sample.py
@@ -0,0 +1,45 @@
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+bucket = "sensors"
+org = "BVP"
+token = "FOxPmFk6mKxzuqlWUC5AKwWZzXl1L8LIQ3wFhqL2l3DAMrSxPkAoB11vkfpijHGzhtDCZY4tsd3pvGxOjQFGMA=="
+url="http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+
+# Query script
+query_api = client.query_api()
+
+query = 'from(bucket:"sensors")\
+|> range(start: -1h)\
+|> filter(fn:(r) => r._measurement == "sensors")\
+|> filter(fn:(r) => r.location == "Prague")\
+|> filter(fn:(r) => r._field == "temperature")'
+
+result = query_api.query(org=org, query=query)
+results = []
+for table in result:
+    for record in table.records:
+        results.append((record.get_field(), record.get_value(), record.get_time()))
+
+print(results)
+print(result)
+print(result[0].records)
+print(result[0].records[0].values)
+print(result[0].records[0])
+
+# The Flux object provides the following methods for accessing your data:
+
+# get_measurement(): Returns the measurement name of the record.
+# get_field(): Returns the field name.
+# get_value(): Returns the actual field value.
+# values: Returns a map of column values.
+# values.get("<your tag>"): Returns a value from the record for given column.
+# get_time(): Returns the time of the record.
+# get_start(): Returns the inclusive lower time bound of all records in the current table.
+# get_stop(): Returns the exclusive upper time bound of all records in the current table.
---
+++ python_scripts/write_sample.py
@@ -0,0 +1,19 @@
+import influxdb_client, time
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+bucket = "sensors"
+org = "BVP"
+token = "FOxPmFk6mKxzuqlWUC5AKwWZzXl1L8LIQ3wFhqL2l3DAMrSxPkAoB11vkfpijHGzhtDCZY4tsd3pvGxOjQFGMA=="
+url="http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+
+write_api = client.write_api(write_options=SYNCHRONOUS)
+for i in range(10):
+    p = influxdb_client.Point("sensors").tag("location", "Prague").field("temperature", (25.3 + i) * (i + 1))
+    write_api.write(bucket=bucket, org=org, record=p)
+    time.sleep(10)
