1a8ee949ea2832c3825c281f0c7df0a9b3d990ea
su-re-sh-75 <suresh22072003@gmail.com>
2024-04-26 09:58:07 +0530

    added: karthi token

--- python_scripts/write_1.py
+++ python_scripts/write_1.py
@@ -11,9 +11,9 @@ HIGH = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin,GPIO.IN)
 print("Running")
-bucket = "PiData"
-org = "BabyMonitoringApp"
-token = "RrvK5MU_w3I9fCn6VbEDlPc5DMVz-SKGfRKIwHFPAxkd-D-Wl8B__GudbqHh_ZkxBgEIjAjj0jaY7j8tZyKEtQ=="
+bucket = "suresh"
+org = "BVP"
+token = "FOxPmFk6mKxzuqlWUC5AKwWZzXl1L8LIQ3wFhqL2l3DAMrSxPkAoB11vkfpijHGzhtDCZY4tsd3pvGxOjQFGMA"
 
 url = "http://192.168.146.69:8000"
 
@@ -21,15 +21,11 @@ client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
 write_api =  client.write_api(write_options = SYNCHRONOUS)
 
-MOISTURE = 90 
-TEMPERATURE = 30
-LOUDNESS = 0
-MOVEMENT = 1
 while(True):
     output = GPIO.input(pin)
     point = (
-        influxdb_client.Point("PI_TEST")
-        .tag("PI","0")
+        influxdb_client.Point("sensors")
+        .tag("username","suresh")
         .field("Loudness", 1 if output == GPIO.HIGH else 0)
     )
     write_api.write(bucket=bucket,org=org,record = point)
